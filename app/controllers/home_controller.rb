b62dc7b10a8315b9ae88c4ecae1bae924c05e78d
Kojo Kwakye <kodeon6@gmail.com>
2024-06-18 21:55:56 +0100

    Initialise repository

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  has_many_attached :images
+  has_rich_text :content
+end
